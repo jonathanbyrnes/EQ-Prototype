d5f17209dc8df954d6f2acca3d8184fbe53790e5
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-02-21 18:42:50 +0000

    Initial commit

---
+++ src/main/java/byrnes/jonathan/eqprototype/EqprototypeApplication.java
@@ -0,0 +1,13 @@
+package byrnes.jonathan.eqprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqprototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqprototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/byrnes/jonathan/eqprototype/EqprototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package byrnes.jonathan.eqprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqprototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
