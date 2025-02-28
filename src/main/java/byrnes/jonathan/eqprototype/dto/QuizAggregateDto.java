118c8497629bad7d7b76c37e98c03bb435b66ca3
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-02-28 12:39:55 +0000

    fix: add getter and setter annotations to dto

--- src/main/java/byrnes/jonathan/eqprototype/dto/QuizAggregateDto.java
+++ src/main/java/byrnes/jonathan/eqprototype/dto/QuizAggregateDto.java
@@ -1,8 +1,12 @@
 package byrnes.jonathan.eqprototype.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class QuizAggregateDto {
 
